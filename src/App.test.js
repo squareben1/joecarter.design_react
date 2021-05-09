6473ae85ff55f9f468d4b85cd8d15db2eae37b32
Squareben1 <bengittins@live.com>
2021-05-09 21:49:03 +0100

    Basic text only page

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders email address', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/joecarterdesign@gmail.com/i);
   expect(linkElement).toBeInTheDocument();
 });

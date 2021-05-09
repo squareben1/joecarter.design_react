b7f171c81e8f580ffd4df9aace6f35ed6b3d43a2
Squareben1 <bengittins@live.com>
2021-05-09 22:19:48 +0100

    Remove white space at top

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders email address', () => {
   render(<App />);
-  const linkElement = screen.getByText(/joecarterdesign@gmail.com/i);
-  expect(linkElement).toBeInTheDocument();
+  const email = screen.getByText(/joecarterdesign@gmail.com/i);
+  expect(email).toBeInTheDocument();
 });
